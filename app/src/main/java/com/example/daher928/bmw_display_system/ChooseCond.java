922585f94054056853f42db08b88e267ac102ed9
Daher Daher <daher928@campus.technion.ac.il>
2018-06-16 15:40:11 +0300

    Testing commit and checking out

--- app/src/main/java/com/example/daher928/bmw_display_system/ChooseCond.java
+++ app/src/main/java/com/example/daher928/bmw_display_system/ChooseCond.java
@@ -3,14 +3,30 @@ package com.example.daher928.bmw_display_system;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.Toast;
 
-public class ChooseCond extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ChooseCond extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    final int SPINNERS_COUNT = 4;
+    List<String> spinner_items_list;
+    String[] spinner_chosen_items = {"nil","nil","nil","nil"};
+    ArrayAdapter<String> adapter;
+    Spinner[] spinners = new Spinner[SPINNERS_COUNT];
+    int last_active_spinner = 0;
+    Button[] rmv_buttons = new Button[SPINNERS_COUNT];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,27 +42,124 @@ public class ChooseCond extends AppCompatActivity {
 
         });
 
-        LinearLayout row2 = (LinearLayout) findViewById(R.id.linear_layout);
-        Button ivBowl = new Button(this);
-        ivBowl.setText("Dynamically Added");
-        LinearLayout.LayoutParams layoutParams = new  LinearLayout.LayoutParams(250, 50);
-        layoutParams.setMargins(5, 3, 0, 0); // left, top, right, bottom
-        ivBowl.setLayoutParams(layoutParams);
-        row2.addView(ivBowl);
+        spinners[0] = findViewById(R.id.spinner0);
+        spinners[1] = findViewById(R.id.spinner1);
+        spinners[2] = findViewById(R.id.spinner2);
+        spinners[3] = findViewById(R.id.spinner3);
 
-        Button ivBowl2 = new Button(this);
-        ivBowl2.setText("Dynamically Added 2");
-        LinearLayout.LayoutParams layoutParams2 = new  LinearLayout.LayoutParams(250, 50);
-        layoutParams.setMargins(5, 3, 0, 0); // left, top, right, bottom
-        ivBowl2.setLayoutParams(layoutParams);
-        row2.addView(ivBowl2);
 
-        Button ivBowl3 = new Button(this);
-        ivBowl3.setText("Dynamically Added 3");
-        LinearLayout.LayoutParams layoutParams3 = new  LinearLayout.LayoutParams(250, 50);
-        layoutParams.setMargins(5, 3, 0, 0); // left, top, right, bottom
-        ivBowl3.setLayoutParams(layoutParams);
-        row2.addView(ivBowl3);
+        rmv_buttons[0] = null;
+        rmv_buttons[1] = findViewById(R.id.remove_button1);
+        rmv_buttons[2] = findViewById(R.id.remove_button2);
+        rmv_buttons[3] = findViewById(R.id.remove_button3);
 
+        rmv_buttons[1].setVisibility(View.INVISIBLE);
+        rmv_buttons[2].setVisibility(View.INVISIBLE);
+        rmv_buttons[3].setVisibility(View.INVISIBLE);
+
+//        for(int i=1; i<SPINNERS_COUNT; i++){
+//            final int finalI = i;
+//            rmv_buttons[i].setOnClickListener(new View.OnClickListener() {
+//                public void onClick(View button) {
+//                    if (button.isPressed()) {
+//                        spinners[finalI].setVisibility(View.INVISIBLE);
+//                        last_active_spinner--;
+//                        if(last_active_spinner!=0)
+//                            rmv_buttons[last_active_spinner].setVisibility(View.VISIBLE);
+//                        for(int j=1; j<SPINNERS_COUNT; j++){
+//                            if(j!=last_active_spinner)
+//                                rmv_buttons[j].setVisibility(View.INVISIBLE);
+//                        }
+//                        recover_selected(finalI);
+//                    }
+//                }
+//            });
+//        }
+
+
+        String[] spinner_items = {"<Select>", "Condition1", "Condition2", "Condition3", "Condition4"};
+
+
+        spinner_items_list = new ArrayList<>(Arrays.asList(spinner_items));
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, spinner_items_list);
+
+        for (Spinner spinner : spinners) {
+            spinner.setAdapter(adapter);
+            spinner.setOnItemSelectedListener(this);
+            if (spinner.getId() != R.id.spinner0)
+                spinner.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        Spinner curr_spinner = (Spinner) parent;
+        final int s_idx = curr_spinner.getId() - spinners[0].getId();
+
+        if (parent.getItemAtPosition(pos).toString() != "<Select>") {
+            if(spinner_chosen_items[s_idx].equals("nil")) { //New Selection
+                if (s_idx != SPINNERS_COUNT - 1) {
+                    spinners[s_idx+1].setVisibility(View.VISIBLE);
+                    last_active_spinner++;
+                }
+                spinner_chosen_items[s_idx] = parent.getItemAtPosition(pos).toString();
+                spinner_items_list.remove(parent.getItemAtPosition(pos).toString());
+            }else{  //Changed selection
+                String recovered = spinner_chosen_items[s_idx];
+                spinner_items_list.add(recovered);
+                spinner_chosen_items[s_idx] = parent.getItemAtPosition(pos).toString();
+                spinner_items_list.remove(parent.getItemAtPosition(pos).toString());
+            }
+
+            if(last_active_spinner < s_idx)
+                last_active_spinner = s_idx;
+            if(last_active_spinner >= 1) {
+                rmv_buttons[last_active_spinner].setVisibility(View.VISIBLE);
+                for(int i=1; i<SPINNERS_COUNT; i++){
+                    if(i!=last_active_spinner)
+                        rmv_buttons[i].setVisibility(View.INVISIBLE);
+                }
+
+            }
+        }
+
+//TODO:remove_button action
+//        if(s_idx!=0){
+//            for(int i=1; i<SPINNERS_COUNT; i++){
+//                final int finalI = i;
+//                Log.i("finalI=", Integer.toString(finalI));
+//                rmv_buttons[finalI].setOnClickListener(new View.OnClickListener() {
+//                    public void onClick(View button) {
+//                        if (button.isPressed()) {
+//                            spinners[finalI].setVisibility(View.INVISIBLE);
+//                            last_active_spinner--;
+//                            if(finalI-1!=0)
+//                                rmv_buttons[finalI-1].setVisibility(View.VISIBLE);
+//                            for(int j=1; j<SPINNERS_COUNT; j++){
+//                                if(j!=finalI-1)
+//                                    rmv_buttons[j].setVisibility(View.INVISIBLE);
+//                            }
+//                            recover_selected(s_idx);
+//                        }
+//                    }
+//                });
+//            }
+
+//        }
+
+
+
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    void recover_selected(int spinner_idx){
+        String recovered = spinner_chosen_items[spinner_idx];
+        spinner_items_list.add(recovered);
+        //spinner_chosen_items[spinner_idx] = "nil";
     }
 }
